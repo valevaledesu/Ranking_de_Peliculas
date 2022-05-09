0400c2aabf5879d978714716fba30408f0ec708b
RogerLopez1111 <rogerlopez1111@gmail.com>
2022-05-08 23:58:29 -0500

    Primer intento
    correlo porfa, sigo son poder

--- src/Ranking.java
+++ src/Ranking.java
@@ -5,10 +5,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Ranking extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtBuscaPorPelicula;
 
 	/**
 	 * Launch the application.
@@ -37,8 +47,33 @@ public class Ranking extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("LOGIN");
-		lblNewLabel.setBounds(274, 70, 61, 16);
-		contentPane.add(lblNewLabel);
+		txtBuscaPorPelicula = new JTextField();
+		txtBuscaPorPelicula.setBorder(new RoundedBorder(20));
+		txtBuscaPorPelicula.setText("Busca por pelicula o director");
+		txtBuscaPorPelicula.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				txtBuscaPorPelicula.setText("");
+			}
+		});
+		
+		txtBuscaPorPelicula.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+			}
+		});
+		txtBuscaPorPelicula.setBounds(10, 11, 461, 20);
+		contentPane.add(txtBuscaPorPelicula);
+		txtBuscaPorPelicula.setColumns(10);
+		
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//hacer consulta, primero nombre de pelicula, en caso de no encontrar, por nombre de director
+			}
+		});
+		btnNewButton.setBounds(481, 10, 89, 23);
+		contentPane.add(btnNewButton);
 	}
 }
